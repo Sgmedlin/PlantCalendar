0d927a1857d17b971902170abcaa5ffcce28cae2
Sam Medlin <sammedlin@Annas-MBP.zyxel.com>
2019-07-25 14:27:13 -0400

    Calendar Stuff

--- plants/views.py
+++ plants/views.py
@@ -7,27 +7,56 @@ import pickle
 import os.path
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
-from social_auth.models import UserSocialAuth
+from social_django.models import UserSocialAuth
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def login(request):
-	return render(request, 'login.html') 
+    return render(request, 'login.html')
+
 
 @login_required()
 def logout(request):
-	auth_logout(request)
-	return render(request, 'home.html')
+    auth_logout(request)
+    return render(request, 'home.html')
+
 
 @login_required()
 def create_event(request):
-  creds = UserSocialAuth.get_social_auth_for_user(request.user).get().tokens
+    event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2019-07-31T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2019-07-31T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+    creds = UserSocialAuth.get_social_auth_for_user(request.user).get().tokens
     # Save the credentials for the next run
-  service = build('calendar', 'v3', credentials=creds)
-
-  event = service.events().insert(calendarId='primary', body=event).execute()
-  print('Event created: %s' % (event.get('htmlLink')))
-
-  return render(request, 'home.html')
+    service = build('calendar', 'v3', credentials=creds)
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+    return render(request, 'home.html')
 
